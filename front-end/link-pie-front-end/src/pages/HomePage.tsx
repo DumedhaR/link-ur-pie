54831fe3d5cd3dfe40363a5e21d947901ee88652
DumedhaR <pererarukshan45@gmail.com>
2025-05-06 14:14:10 +0530

    changed home page sections gaps and spacings

--- front-end/link-pie-front-end/src/pages/HomePage.tsx
+++ front-end/link-pie-front-end/src/pages/HomePage.tsx
@@ -41,7 +41,7 @@ const HomePage: React.FC = () => {
       <section className="hero-section h-[calc(100vh-64px)]">
         <Hero CoverImg={HeroImage} />
       </section>
-      <section className='feature-section px-4 md:px-36 pt-20 pb-22'>
+      <section className='feature-section px-4 md:px-36 pt-20 pb-24'>
         <h2 className="text-3xl md:text-3xl font-bold text-gray-800 text-center mb-12 capitalize">What We Offer</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {features.map((feature, index) => (
@@ -49,7 +49,7 @@ const HomePage: React.FC = () => {
           ))}
         </div>
       </section>
-      <section className="how-it-works-section px-4 md:px-36 pt-20 pb-22 bg-white">
+      <section className="how-it-works-section px-4 md:px-36 pt-20 pb-24 bg-white">
           <h2 className="text-3xl md:text-3xl font-bold text-gray-800 text-center mb-12 capitalize">How it works</h2>
           <HowItWorks/>
       </section>
